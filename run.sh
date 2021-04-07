337e67375997e1980a916303ba8143954afeb8ca
Aleks Golubyatnikov <aleks.golubyatnikov@gmail.com>
2021-04-07 13:08:30 +0300

    Fix errors

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 DateTime=$(date +"%Y/%m/%d %H:%M:%S")
 echo "$DateTime start installation..." >> instalation.log
 
-#docker
+#Docker
 sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release 2>&1 >> instalation.log 2>&1
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg >> instalation.log 2>&1
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -16,14 +16,13 @@ sudo usermod -aG docker golubyatnikov >> instalation.log 2>&1
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" 
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl >> instalation.log 2>&1
 
-#minikube
+#Minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb >> instalation.log 2>&1
-minikube start >> instalation.log 2>&1
 
 DateTime=$(date +"%Y/%m/%d %H:%M:%S")
 echo "$DateTime end installation..." >> instalation.log
 
-#sudo usermod -aG docker ${USER} >> instalation.log 2>&1
-#logout
+#minikube start
+
 
