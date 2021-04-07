1a5451aa7b8e885f5cf0bd02668bd1a0817e4dd4
Aleks Golubyatnikov <aleks.golubyatnikov@gmail.com>
2021-04-07 12:35:01 +0300

    Fix errors

--- run.sh
+++ run.sh
@@ -9,6 +9,9 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] 
 sudo apt-get update >> instalation.log 2>&1
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io >> instalation.log 2>&1
 
+sudo groupadd docker >> instalation.log 2>&1
+sudo usermod -aG docker golubyatnikov >> instalation.log 2>&1
+
 #kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" 
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl >> instalation.log 2>&1
@@ -16,10 +19,6 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl >> instalati
 #minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb >> instalation.log 2>&1
-
-sudo groupadd docker >> instalation.log 2>&1
-sudo usermod -aG docker golubyatnikov >> instalation.log 2>&1
-
 minikube start >> instalation.log 2>&1
 
 DateTime=$(date +"%Y/%m/%d %H:%M:%S")
