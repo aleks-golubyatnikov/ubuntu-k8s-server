c4e40aff984a068ea8275009c3b3faf9705f9d36
Aleks Golubyatnikov <aleks.golubyatnikov@gmail.com>
2021-04-07 11:27:37 +0300

    Add minikube installation

--- run.sh
+++ run.sh
@@ -9,13 +9,17 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] 
 sudo apt-get update >> instalation.log
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io >> instalation.log
 
+sudo groupadd docker >> instalation.log
+sudo usermod -aG docker ${USER} >> instalation.log
+
 #kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" 
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl >> instalation.log
 
 #minikube
-#curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-#minikube start  >> instalation.log
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_arm64.deb
+sudo dpkg -i minikube_latest_arm64.deb
+minikube start >> instalation.log
 
 DateTime=$(date +"%Y/%m/%d %H:%M:%S")
 echo "$DateTime end installation..." >> instalation.log
